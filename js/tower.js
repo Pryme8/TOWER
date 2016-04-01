71eedb8ff3ac56ae3d06090f1efac3c41a05a6c6
Andrew Butt <pryme8@gmail.com>
2016-04-01 11:12:02 -0700

    Update

---
+++ js/tower.js
@@ -0,0 +1,21 @@
+// TOWER 3D - Author: Andrew V Butt Sr. Pryme8@gmail.com 2016
+TOWER = function(){
+	this.CORE = {
+		engine : null,
+		scenes : [],
+		objects : [],	
+		};
+	this.DOM = {};
+	this._init();	
+}
+
+TOWER.prototype._init = function(){
+	this.DOM.Master = $('tower');
+	this.DOM.canvas = $(TOWER.ELEMENTS.canvas);
+	
+
+}
+
+TOWER.ELEMENTS = {
+	canvas : '<canvas id="renderCanvas"></canvas>',
+};
